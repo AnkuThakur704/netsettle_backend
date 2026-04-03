c8bb3337fe154f8ae97ca685d5e07b43416ee5b7
AnkuThakur704 <ankushanku704@gmail.com>
2026-04-03 19:09:17 +0530

    origin issue

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ app.use(cors({
     origin:['http://localhost:5173',"https://netsettle-frontend.vercel.app"],
     credentials:true
 }))
+app.options('*', cors())
 app.set("trust proxy", 1);
 app.use(cookieParser())
 app.use(express.json())
